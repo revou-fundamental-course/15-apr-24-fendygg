b15841a14517efec62a3afcf8c3c5127c29dd970
fendygg <166905541+fendygg@users.noreply.github.com>
2024-04-23 00:54:14 +0700

    Add files via upload

--- konversisuhu/js/script.js
+++ konversisuhu/js/script.js
@@ -1,1 +1,11 @@
 //javascript
+
+const inputcelcius = document.getElementById("inputcelcius");
+let outputfahrenheit = document.getElementById("outputfahrenheit");
+
+function convert(){
+
+    let inputcelcius = document.getElementById("inputcelcius").value;
+    outputfahrenheit.textContent = inputcelcius * 9/5 + 32
+
+}
